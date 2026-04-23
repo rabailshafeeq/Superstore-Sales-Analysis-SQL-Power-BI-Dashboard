fe88f46b2024a225969626f96d8d084ed1ee977f
Rabail Shafeeq <166241999+rabailshafeeq@users.noreply.github.com>
2026-04-23 15:24:06 +0500

    Update header comments in SQL analysis script

--- Superstore_Sales_Analysis.sql
+++ Superstore_Sales_Analysis.sql
@@ -1,7 +1,5 @@
 ﻿/*
 ===============================================================================
- Project:    Superstore Sales Performance Analysis
- Author:     Mohan Kumar
  Description:
    This SQL script provides an in-depth analysis of the Superstore sales 
    dataset. It includes sales, profit, quantity, shipping trends, customer 
